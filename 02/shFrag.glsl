bc4d916e00b69e3377343733445dc90d630806b2
joonsooan <joonsooan@gmail.com>
2025-09-19 16:10:53 +0900

    Added 06_FlipTriangle

--- 02/shFrag.glsl
+++ 02/shFrag.glsl
@@ -1,9 +1,9 @@
 #version 300 es
 precision mediump float;
 
-in vec3 ourColor;
 out vec4 FragColor;
+uniform vec4 uColor;
 
 void main() {
-    FragColor = vec4(ourColor, 1.0);
+    FragColor = uColor;
 }
--- 02/shVert.glsl
+++ 02/shVert.glsl
@@ -1,11 +1,9 @@
 #version 300 es
 
-layout(location = 0) in vec3 aPos;
-layout(location = 1) in vec3 aColor;
+layout (location = 0) in vec3 aPos;
 
-out vec3 ourColor;
+uniform float verticalFlip;
 
 void main() {
-    gl_Position = vec4(aPos, 1.0);
-    ourColor = aColor;
+    gl_Position = vec4(aPos[0], aPos[1] * verticalFlip, aPos[2], 1.0);
 }
